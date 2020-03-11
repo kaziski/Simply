0ce0054d71bcf159227c1938523da15b45f51606
Kazumi Karbowski <kazkazinus@gmail.com>
2020-03-10 22:24:06 -0400

    Let admin create new users To avoid Already signed in error message, I routed to User resource to create a new user separate from device user. It has to go through different route, controller and view to create a new user as admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :destroy]
-  before_action :redirect_unless_admin, only: [:edit]
+  before_action :redirect_unless_admin, only: [:new, :create, :edit]
 
   def index
     @users = User.all
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      flash[:danger] = @user.errors.full_messages.to_sentence
+      redirect_to new_user_registration_path
+    end
+  end 
 
   def update
     params[:user].delete(:password) if params[:user][:password].blank?
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Custom route for creating a new user - admin only
   devise_scope :user do
-  #   get "/create_new_user" => "users/registrations#new", as: "create_new_user" 
-    post "/new_user" => "users/registrations#create"
+    post "/create_new_user" => "users/registrations#create"
   end
 
-  resources :users, only: [:index, :edit, :update, :destroy]
+  resources :users
+
+  post "/admin_create_new_user" => "users#create"
 
   root to: "welcome#index"
 end
